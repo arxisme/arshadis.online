08b5b47ff349a3034f6b75159995076f2e0795f7
arxisme <arsh11100arsh@gmail.com>
2024-07-31 10:20:29 +0530

    dsfsa

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -8,7 +8,7 @@ const HamburgerMenu = () => {
   return (
     <div className="relative">
       <Hamburger toggled={isOpen} toggle={setIsOpen} size={20} easing="ease-in" duration={0.5} />
-      <div className={`absolute right-0 mt-2 bg-base-100 w-48 text-xs rounded-lg drop-shadow-lg z-50 transition-all duration-500 ease-in-out transform ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'}`}>
+      <div className={`absolute right-0 mt-2 bg-base-100 w-48 text-xs rounded-lg drop-shadow-lg z-50 transition-all duration-300 ease-in-out transform ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-1'}`}>
         <ul className="py-1">
           <li className="px-4 py-2 text-info underline"><a href="/blogs">Blogs</a></li>
           <li className="px-4 py-2 underline"><a href="#home">Home</a></li>
