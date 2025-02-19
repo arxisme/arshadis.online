4764e4a0631dad714637e1b32a993ee3c1e7f555
Arshad Ahmed <72617441+arxisme@users.noreply.github.com>
2025-02-20 00:35:18 +0530

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="min-h-screen md:flex w-full justify-around">
       <div className="box-content flex flex-col rounded-xl max-w-screen-2xl max-2xl:max-w-screen-lg w-full">
-      <div className='w-full fixed bg-white left-1/2 transform -translate-x-1/2'>nav</div>
+      <div className='w-full fixed bg-white left-1/2 transform -translate-x-1/2'>Work in not progress, Sorry!</div>
       </div>
     </main>
   );
